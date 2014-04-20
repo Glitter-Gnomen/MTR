544e6bb68c0e13c98435678270fb8a55396e82b4
StoltZ <troels.hagensen@gmail.com>
2014-04-20 22:01:37 +0200

    Invers polish notation calc
    the first part of my invers polish notation calculator - as part of the
    'ultimate calculator'.

--- test8/test8/Program.cs
+++ test8/test8/Program.cs
@@ -1,16 +1,64 @@
-﻿using System;
+﻿//Invers Polish Notation Calculator by T. L. Hagensen
+//Version 1.0 for use in console
+
+using System;
 
 namespace test8
 {
-	class MainClass
+	public class MainClass
 	{
-		public static void Main (string[] args)
+		public class extension
 		{
-			Console.WriteLine ("Testing Testing!");
+			//Used classes
+			addition add = new addition ();
+			subtraction sub = new subtraction ();
 
-			Console.WriteLine ("Iphone Game");
+			//Variables
+			private static double var1; //Used for input 1
+			private static double var2; //Used for input 2
+			private static string var3; //Used to specify the desired calculation operation
 
-            Console.WriteLine("Does it work?");
+
+			//Main method
+			public static void Main (string[] args)
+			{
+				//First input
+				Console.WriteLine ("Input 1 here:");
+				Console.WriteLine ("Enter a number: ");
+				var1 = Convert.ToDouble (Console.ReadLine ());
+
+				extension ext = new extension ();
+				ext.Calcs (var1);
+			}
+
+			//Recursive method to take last input and what to do with the two inputs
+			public void Calcs (double calcVar1)
+			{
+				//Second input
+				Console.WriteLine ("Input 2 here:");
+				Console.WriteLine ("Enter a number: ");
+				var2 = Convert.ToDouble (Console.ReadLine ());
+
+
+				//User picks desired mathematical operation
+				Console.WriteLine ("Select calculation");
+				var3 = (Console.ReadLine ());
+
+
+				//Switch case for the different calculation operations
+				switch (var3) {
+
+				//Addition
+				case "+": 
+					add.adding (calcVar1, var2);
+					break;
+
+					//Subtraction
+				case "-":
+					sub.subbing (calcVar1, var2);
+					break;
+				}
+			}
 		}
 	}
 }
---
+++ test8/test8/addition.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace test8
+{
+    public class addition : MainClass
+    {
+        public void adding(double input1, double input2)
+        {
+            double result;
+
+            Console.WriteLine(input1 + input2);
+            result = input1 + input2;
+            extension ext = new extension();
+            ext.Calcs(result);
+        }
+    }
+}
